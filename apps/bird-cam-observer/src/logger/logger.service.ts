fed2ce47c5d50dfa46fef28880c12f2c524a4bb1
Robert Werner <robert.werner87@gmail.com>
2023-03-05 20:55:30 +0100

    fix logging

--- apps/bird-cam-observer/src/logger/logger.service.ts
+++ apps/bird-cam-observer/src/logger/logger.service.ts
@@ -1,22 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, LoggerService as Logger } from '@nestjs/common';
 import { createLogger, transports, format } from 'winston';
 
 @Injectable()
-export class LoggerService {
-
-  private readonly customFormat = format.printf(({ level, message, timestamp }) => {
-    return `${timestamp} [${level}]: ${message}`;
-  });
-
+export class LoggerService implements Logger {
+  private readonly customFormat = format.printf(
+    ({ level, message, timestamp }) => {
+      return `${timestamp} [${level}]: ${message}`;
+    }
+  );
 
   private readonly logger = createLogger({
     transports: [
       new transports.File({
-        filename: '../log.log',
+        filename: 'birdcam.log',
         format: format.combine(format.timestamp(), this.customFormat),
       }),
-      new transports.Console()
-    ]
+      new transports.Console(),
+    ],
   });
 
   info(message: string): void {
@@ -27,4 +27,11 @@ export class LoggerService {
     this.logger.error(message);
   }
 
+  log(message: string): void {
+    this.logger.info(message);
+  }
+
+  warn(message: string): void {
+    this.logger.warn(message);
+  }
 }
--- apps/bird-cam-observer/src/main.ts
+++ apps/bird-cam-observer/src/main.ts
@@ -7,9 +7,13 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { LoggerService } from './logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(LoggerService));
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
