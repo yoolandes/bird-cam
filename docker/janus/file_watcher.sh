03c30945a638b5a98a9a40ec99277995bd382902
Robert Werner <robert.werner87@gmail.com>
2023-04-19 19:46:53 +0200

    brightness filewatcher

--- docker/janus/file_watcher.sh
+++ docker/janus/file_watcher.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-file_move() {
-    if [ $1 = "brightness-video-0.mjr" ]
-    then
-        /opt/janus/bin/janus-pp-rec /home/recordings/brightness-video-0.mjr /home/recordings/brightness.mp4
-        ffmpeg -skip_frame nokey -i /home/recordings/brightness.mp4 -vsync 0 -update true /home/recordings/brightness.png -y
-        brightness=`convert /home/recordings/brightness.png -colorspace Gray -format "%[fx:mean]" info:`
-        curl -X POST -H "Content-Type: application/json" -d "{\"brightness\": $brightness}" http://192.168.178.59:3333/api/brightness
-    fi
+frameCount = 0
+
+handle_brightness() {
+    /opt/janus/bin/janus-pp-rec /home/recordings/recording-video-0.mjr.tmp /home/recordings/recording.mp4
+    ffmpeg -skip_frame nokey -i /home/recordings/recording.mp4 -vsync 0 -update true /home/recordings/recording.png -y
+    brightness=`convert /home/recordings/recording.png -colorspace Gray -format "%[fx:mean]" info:`
+    curl -X POST -H "Content-Type: application/json" -d "{\"brightness\": $brightness}" http://192.168.178.59:3333/api/brightness
 }
+
+# handle_motion() {
+#     /opt/janus/bin/janus-pp-rec /home/recordings/motion-video-0.mjr /home/recordings/motion.mp4
+#     ffmpeg -skip_frame nokey -i /home/recordings/motion.mp4 -vsync 0 -update true /home/recordings/motion.jpg -y
+#     #curl -X POST -H "Content-Type: application/json" -d "{\"brightness\": $brightness}" http://192.168.178.59:3333/api/brightness
+# }
 
 
 inotifywait -q -m -r -e move /home/recordings/ | while read DIRECTORY EVENT FILE; do
     case $EVENT in
-        MOVE*)
-            file_move "$FILE"
+        
+        CREATE)
+            $frameCount = 0
+        ;;
+        
+        MODIFY)
+            $frameCount++
+            if [ $frameCount = 24 ]
+            then
+                handle_brightness
+            fi
+        ;;
+        
+        CLOSE)
+            if [ $frameCount <= 24 ]
+            then
+                handle_brightness
+            fi
         ;;
     esac
 done
