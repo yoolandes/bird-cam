5adec9ebcd32f5e71f6d9384fc056a498dc5ad90
Robert Werner <robert.werner87@gmail.com>
2023-04-25 08:48:32 +0200

    yes

--- docker/janus/file_watcher.sh
+++ docker/janus/file_watcher.sh
@@ -6,17 +6,17 @@ start=0
 last_motion=0
 
 handle_brightness() {
-    /opt/janus/bin/janus-pp-rec /home/recordings/recording-video-0.mjr.tmp /home/recordings/recording.mp4
+    /opt/janus/bin/janus-pp-rec /home/recordings/$1 /home/recordings/recording.mp4
     ffmpeg -skip_frame nokey -i /home/recordings/recording.mp4 -vsync 0 -update true /home/recordings/recording.jpg -y
     brightness=`convert /home/recordings/recording.jpg -colorspace Gray -format "%[fx:mean]" info:`
-    curl -X POST -H "Content-Type: application/json" -d "{\"brightness\": $brightness}" http://192.168.178.59:3333/api/brightness
+    curl -X POST -H "Content-Type: application/json" -d "{\"brightness\": $brightness}" http://192.168.178.80:3333/api/brightness
 }
 
 handle_motion() {
     echo $package_count
     package_count=$((`/opt/janus/bin/janus-pp-rec /home/recordings/recording-video-0.mjr.tmp /home/recordings/motion.mp4 -i $package_count | grep -w 'RTP' | grep -o -E '[0-9]+'` + $package_count ))
     ffmpeg -skip_frame nokey -i /home/recordings/motion.mp4 -vsync 0 -update true /home/recordings/motion.jpg -y
-    curl -X POST -F file=@/home/recordings/motion.jpg -F date=$(date -Is) http://192.168.178.59:3333/api/snapshot
+    curl -X POST -F file=@/home/recordings/motion.jpg -F date=$(date -Is) http://192.168.178.80:3333/api/snapshot
 }
 
 
@@ -25,33 +25,38 @@ inotifywait -q -m /home/recordings/ | while read DIRECTORY EVENT FILE; do
     then
         case $EVENT in
             CREATE)
+                echo "create"
+                echo $frame_count
                 frame_count=0
                 package_count=0
                 start=`date +%s`
-		last_motion=0
+                last_motion=0
                 
             ;;
             MODIFY)
-#		echo $frame_count
+                echo "modify"
+                echo $frame_count
                 frame_count=$(( $frame_count + 1 ))
                 
                 if [ $frame_count -eq 24 ]
                 then
-                    handle_brightness
+                    handle_brightness "recording-video-0.mjr.tmp"
                 fi
                 
                 end=`date +%s`
                 runtime=$((end-start))
                 if ! (( $runtime % 5 )) && [ $runtime -ne $last_motion ]
                 then
-		    last_motion=$runtime
-		    handle_motion
+                    last_motion=$runtime
+                    handle_motion
                 fi
             ;;
             CLOSE)
+                echo "close"
+                echo $frame_count
                 if [ $frame_count -lt 24 ]
                 then
-                    handle_brightness
+                    handle_brightness "recording-video-0.mjr"
                 fi
             ;;
         esac
