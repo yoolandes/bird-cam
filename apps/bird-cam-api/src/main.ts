49cf34cf56d295250e30cdbd874e56c17d5714e9
Robert Werner <robert.werner87@gmail.com>
2023-07-29 16:03:31 +0200

    add interceptor

--- apps/bird-cam-api/src/main.ts
+++ apps/bird-cam-api/src/main.ts
@@ -8,12 +8,25 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 import { LoggerService } from '@bird-cam/logger';
+import { HttpService } from '@nestjs/axios';
 
 export * from 'mysql';
 export * from 'ansi-styles';
 
 async function bootstrap() {
+  const httpService = new HttpService();
   const app = await NestFactory.create(AppModule);
+
+  httpService.axiosRef.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      Logger.error(error);
+      throw error;
+    }
+  );
+
   app.useLogger(app.get(LoggerService));
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
