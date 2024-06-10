d6ad92a5a960fb794b7e4485c396b62b37477c08
Robert Werner <robert.werner87@gmail.com>
2024-06-10 13:41:33 +0200

    add notification

--- libs/push-subscriber/ui/src/lib/push-subscriber-button/push-subscriber-button.component.ts
+++ libs/push-subscriber/ui/src/lib/push-subscriber-button/push-subscriber-button.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { PushSubscriberService } from '@bird-cam/push-subscriber/application';
 import { SwPush } from '@angular/service-worker';
+import { ToastController } from '@ionic/angular';
+import { TranslocoService } from '@ngneat/transloco';
+import { skip } from 'rxjs';
 
 @Component({
   selector: 'bird-cam-push-subscriber-button',
@@ -8,15 +11,36 @@ import { SwPush } from '@angular/service-worker';
 })
 export class PushSubscriberButtonComponent {
   protected readonly Notification = Notification;
+
   constructor(
     readonly pushSubscriberService: PushSubscriberService,
-    readonly swPush: SwPush
-  ) {}
+    readonly swPush: SwPush,
+    private readonly toastController: ToastController,
+    private readonly translocoService: TranslocoService
+  ) {
+    this.extracted();
+  }
+
   subscribe(): void {
     this.pushSubscriberService.requestSubscription();
   }
 
   unsubscribe(): void {
     this.pushSubscriberService.unsubscribe();
+  }
+
+  private extracted() {
+    this.pushSubscriberService.showSubscription$
+      .pipe(skip(1))
+      .subscribe((showSubscription) => {
+        const translationKey = showSubscription
+          ? 'pushSubscriber.subscribed'
+          : 'pushSubscriber.unsubscribed';
+        this.toastController
+          .create({
+            message: this.translocoService.translate(translationKey),
+          })
+          .then((toast) => toast.present());
+      });
   }
 }
--- libs/push-subscriber/ui/src/lib/push-subscriber-ui.module.ts
+++ libs/push-subscriber/ui/src/lib/push-subscriber-ui.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { PushSubscriberInfrastructureModule } from '@bird-cam/push-subscriber/infrastructure';
 import { PushSubscriberButtonComponent } from './push-subscriber-button/push-subscriber-button.component';
+import { provideTranslocoScope } from '@ngneat/transloco';
 
 @NgModule({
   imports: [CommonModule, IonicModule, PushSubscriberInfrastructureModule],
   declarations: [PushSubscriberButtonComponent],
   exports: [PushSubscriberButtonComponent],
+  providers: [provideTranslocoScope('push-subscriber')],
 })
 export class PushSubscriberUiModule {}
