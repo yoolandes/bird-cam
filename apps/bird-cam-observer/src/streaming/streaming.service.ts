4e6883b1e1473a551753ede668fb88f890a4396b
Robert Werner <robert.werner87@gmail.com>
2024-07-07 20:09:30 +0200

    show motion activity when zero detects

--- apps/bird-cam-observer/src/streaming/streaming.service.ts
+++ apps/bird-cam-observer/src/streaming/streaming.service.ts
@@ -23,33 +23,39 @@ export class StreamingService {
   constructor(private readonly httpService: HttpService) {}
 
   startStream(): Observable<void> {
-    return this.httpService
-      .post<void>(`${this.baseUrl}/add/cam`, {
-        source: 'rpiCamera',
-        runOnReady:
-          'gst-launch-1.0 rtspclientsink name=s location=rtsp://localhost:$RTSP_PORT/cam_with_audio rtspsrc location=rtsp://127.0.0.1:$RTSP_PORT/$MTX_PATH latency=0 ! rtph264depay ! s. alsasrc ! queue ! audioconvert ! opusenc ! s.sink_1',
+    return this.isStreaming$.pipe(
+      filter((isStreaming) => !isStreaming),
+      switchMap(() => {
+        return this.httpService.post<void>(`${this.baseUrl}/add/cam`, {
+          source: 'rpiCamera',
+          runOnReady:
+            'gst-launch-1.0 rtspclientsink name=s location=rtsp://localhost:$RTSP_PORT/cam_with_audio rtspsrc location=rtsp://127.0.0.1:$RTSP_PORT/$MTX_PATH latency=0 ! rtph264depay ! s. alsasrc ! queue ! audioconvert ! opusenc ! s.sink_1',
+        });
+      }),
+      switchMap(() =>
+        this.isStreaming$.pipe(
+          skip(1),
+          filter((isStreaming) => isStreaming),
+          take(1)
+        )
+      ),
+      map(() => void 0),
+      catchError((err) => {
+        if (err.error === 'path already exists') {
+          return of(void 0);
+        } else {
+          return of(err);
+        }
       })
-      .pipe(
-        switchMap(() =>
-          this.isStreaming$.pipe(
-            skip(1),
-            filter((isStreaming) => isStreaming),
-            take(1)
-          )
-        ),
-        map(() => void 0),
-        catchError((err) => {
-          if (err.error === 'path already exists') {
-            return of(void 0);
-          } else {
-            return of(err);
-          }
-        })
-      );
+    );
   }
 
   stopStream(): Observable<void> {
-    return this.httpService.delete<void>(`${this.baseUrl}/delete/cam`).pipe(
+    return this.isStreaming$.pipe(
+      filter((isStreaming) => isStreaming),
+      switchMap(() =>
+        this.httpService.delete<void>(`${this.baseUrl}/delete/cam`)
+      ),
       switchMap(() =>
         this.isStreaming$.pipe(
           skip(1),
