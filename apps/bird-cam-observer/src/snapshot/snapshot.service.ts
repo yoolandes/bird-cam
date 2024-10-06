1a3ae8fefc5103451dc633673c32630c748b1ca6
Robert Werner <robert.werner87@gmail.com>
2024-10-06 12:31:38 +0200

    create snapshot in docker

--- apps/bird-cam-observer/src/snapshot/snapshot.service.ts
+++ apps/bird-cam-observer/src/snapshot/snapshot.service.ts
@@ -8,24 +8,26 @@ export class SnapshotService {
   getStill(): Promise<string> {
     return new Promise((resolve, reject) => {
       const streamProcess = spawn('rpicam-still', [
-        '--output ./snapshot.jpg',
-        '--width 1920',
-        '--height 1080',
+        '--output',
+        './snapshot.jpg',
+        '--width',
+        '1920',
+        '--height',
+        '1080',
         '--nopreview',
-        '--quality 60',
+        '--quality',
+        '60',
         '--immediate',
       ]);
 
-      streamProcess.stdout.on('data', (data: string) => {
-        this.loggerService.log(`stdout: ${data}`);
-        return resolve(
-          fs.readFileSync('./snapshot.jpg', { encoding: 'base64' })
-        );
-      });
-
-      streamProcess.stderr.on('data', (data: string) => {
-        this.loggerService.log(`stderr: ${data}`);
-        return reject();
+      streamProcess.on('close', (code) => {
+        if (code === 0) {
+          return resolve(
+            fs.readFileSync('./snapshot.jpg', { encoding: 'base64' })
+          );
+        } else {
+          return reject();
+        }
       });
     });
   }
